7c8f1f8efd507cfb1bfbf19157c4b205c9c58e58
charcrede <charcrede@gmail.com>
2023-05-25 18:12:09 +0100

    new commit

--- instructions/effet-js/freshlyrestaurant-js.js
+++ instructions/effet-js/freshlyrestaurant-js.js
@@ -1,9 +1,82 @@
-$(document).ready(function(){
-    $(".slick").slick({
-      infinite:true,
-      slidesToShow:1,
-      dots:true,
-      autoplay:true,
-      autoplaySpeed:1000,
+$(document).ready(function () {
+  $(".slick").slick({
+    infinite: true,
+    slidesToShow: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+  $("#All").click(function () {
+    $("#All").css({
+      "background-color": "var(--rouge)",
+      "color": "#fff"
     });
-  })
+    $("#meat").css({
+      "background-color": "var(--beige)",
+      "color": "#000"
+    });
+    $("#vegan").css({
+      "background-color": "var(--beige)",
+      "color": "#000"
+    });
+    $("#fish").css({
+      "background-color": "var(--beige)",
+      "color": "#000"
+    });
+  });
+  $("#meat").click(function () {
+    $("#meat").css({
+      "background-color": "var(--rouge)",
+      "color": "#fff"
+    });
+    $("#All").css({
+      "background-color": "var(--beige)",
+      "color": "#000"
+    });
+    $("#vegan").css({
+      "background-color": "var(--beige)",
+      "color": "#000"
+    });
+    $("#fish").css({
+      "background-color": "var(--beige)",
+      "color": "#000"
+    });
+  });
+    $("#vegan").click(function () {
+      $("#vegan").css({
+        "background-color": "var(--rouge)",
+        "color": "#fff"
+      });
+      $("#meat").css({
+        "background-color": "var(--beige)",
+        "color": "#000"
+      });
+      $("#All").css({
+        "background-color": "var(--beige)",
+        "color": "#000"
+      });
+      $("#fish").css({
+        "background-color": "var(--beige)",
+        "color": "#000"
+      });
+    });
+    $("#fish").click(function () {
+      $("#fish").css({
+        "background-color": "var(--rouge)",
+        "color": "#fff"
+      });
+      $("#meat").css({
+        "background-color": "var(--beige)",
+        "color": "#000"
+      });
+      $("#vegan").css({
+        "background-color": "var(--beige)",
+        "color": "#000"
+      });
+      $("#All").css({
+        "background-color": "var(--beige)",
+        "color": "#000"
+      });
+    });
+    let mixer = mixitup(".search");
+  });
